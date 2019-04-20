5229de657188b87b922a9ca9587422c78111cbf0
David Biancolin <david.biancolin@gmail.com>
2019-04-19 20:49:16 -0700

    Upgrade run script

--- benchmarks/run-bmark.sh
+++ benchmarks/run-bmark.sh
@@ -1,22 +1,35 @@
 #!/usr/bin/env bash
 
-result=$(make $1.riscv.out)
+bmark=$1
+one_point_threshold=$2
+two_point_threshold=$3
+make $bmark.riscv.out > make.out 2> make.err
 rc=$!
+cat make.out
 
-cycle_count=$(echo $result | grep -oE 'cycles\s*=\s*[0-9]*' | awk -n '{print $3}') 
-instructions=$(echo $result | grep -oE 'instructions\s*=\s*[0-9]*' | awk -n '{print $3}') 
+# Default failing case
+result_template="{ \"score\": 0.0}"
+if [[ $rc -eq 0 ]]; then
+        cycle_count=$(grep -oE 'cycles\s*=\s*[0-9]*' make.out | awk -n '{print $3}')
+        instructions=$(grep -oE 'instructions\s*=\s*[0-9]*' make.out | awk -n '{print $3}')
 
-echo $cycle_count
-echo $instructions
+        score=0.5
+        if [[ $cycle_count -lt $two_point_threshold ]]; then
+           score=2.0
+        elif [[ $cycle_count -lt $one_point_threshold ]]; then
+           score=1.0
+        fi
 
-result_template="
-{ \"score\": 2.0,
-  \"leaderboard\":
-    [
-      {\"name\": \"Cycles\", \"value\": ${cycle_count}, \"order\": \"asc\"}
-    ]
-}"
+        result_template="
+        { \"score\": ${score},
+          \"leaderboard\":
+            [
+              {\"name\": \"Cycles\", \"value\": ${cycle_count}, \"order\": \"asc\"}
+            ]
+        }"
+else
+        echo "Run Failed. Tail of commit log follows:"
+        tail -n10 $bmark.riscv.out
+fi
 
 echo $result_template >> /autograder/results/results.json
-
-exit $!
